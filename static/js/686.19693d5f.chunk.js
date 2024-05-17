9fa399015dd348e20329e63da75f4a97d099a01d
AlexMyron <myrondell@gmail.com>
2024-05-17 18:40:29 +0000

    Deploying to gh-pages from @ AlexMyron/goit-react-woolf-hw-test@b951eba3b2d61d9bd10a6a946e7eab1ca053c1b1 🚀

---
+++ static/js/686.19693d5f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[686],{1686:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"HomePage"})}}}]);
+//# sourceMappingURL=686.19693d5f.chunk.js.map
--- static/js/686.f9103b16.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[686],{686:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"HomePage"})}}}]);
-//# sourceMappingURL=686.f9103b16.chunk.js.map
