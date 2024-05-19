b19639a670f1a9e70e4e73117106f2f5e54b2dd9
Olek Myron <myrondell@gmail.com>
2024-05-19 20:13:18 +0300

    fixed git-pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-woolf-hw-test">
+    <BrowserRouter basename="/react-campervans-rent">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
